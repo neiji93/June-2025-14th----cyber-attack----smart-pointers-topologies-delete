ff6569e328f0b2903f9b061673bb5413002b0483
Nejdet ALTINISIK <nejdet.altinisik@yahoo.fr>
2025-06-15 13:51:28 +0200

    Create ReccjrencesCountEquatkonAlgorithm.h

---
+++ ReccjrencesCountEquatkonAlgorithm.h
@@ -0,0 +1,4 @@
+//reccurences are usefull but not the first thing to do.
+//then, this id an algorithm. in order to keep track ofnmodels,
+//we have ro use common models like qualitative models 
+/)the result would be nano topologies if we use cryogenic devices.
