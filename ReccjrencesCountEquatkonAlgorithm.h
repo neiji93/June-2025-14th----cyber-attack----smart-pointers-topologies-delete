7ca636146f9e63a70b608db86dc9350346247ea6
Nejdet ALTINISIK <nejdet.altinisik@yahoo.fr>
2025-06-15 13:54:08 +0200

    Update ReccjrencesCountEquatkonAlgorithm.h

--- ReccjrencesCountEquatkonAlgorithm.h
+++ ReccjrencesCountEquatkonAlgorithm.h
@@ -2,3 +2,7 @@
 //then, this id an algorithm. in order to keep track ofnmodels,
 //we have ro use common models like qualitative models 
 /)the result would be nano topologies if we use cryogenic devices.
+
+
+  //use oiriwn sruxnet multiple booters.
+  class stuxbootersOn.... : public stuxHeuristicsOne
